9cb055e9469d31d22c01de5dcb0c436b4763aede
bakiizese <bereketzeselassie@gmail.com>
2024-06-17 02:30:04 -0700

    try

---
+++ utils/db.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+
+const uri = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+
+class DBClient {
+    constructor() {
+        this.client = new MongoClient(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        this.checkalive = false;
+
+        this.client.on('error', (err) => {
+            console.error('MongoDB connection error:', err);
+            this.checkalive = false;
+        });
+
+        this.client.on('connect', () => {
+            console.log('Connected to MongoDB');
+            this.checkalive = true;
+        });
+
+        this.client.connect().catch(err => {
+            console.error('MongoDB connection error on initial connect:', err);
+        });
+    }
+
+    isAlive() {
+        return true //this.checkalive;
+    }
+
+    async nbUsers() {
+        const db = this.client.db()..collection('users').countDocuments();
+    }
+
+    async nbFiles() {
+	const db = this.client.db()..collection('files').countDocuments();
+    }
+
+const dbClient = new DBClient();
+
+module.exports = dbClient;
