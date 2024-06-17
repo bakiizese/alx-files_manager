c4bb41d82e25b1a635891ce248a9675a5abe8e67
bakiizese <bereketzeselassie@gmail.com>
2024-06-17 03:17:00 -0700

    mongodb connection" "

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
 const DB_HOST = process.env.DB_HOST || 'localhost';
@@ -8,39 +7,40 @@ const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
 const uri = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
 class DBClient {
-    constructor() {
-        this.client = new MongoClient(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        this.checkalive = false;
+  constructor() {
+    this.client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    this.checkalive = false;
 
-        this.client.on('error', (err) => {
-            console.error('MongoDB connection error:', err);
-            this.checkalive = false;
-        });
+    this.client.on('error', (err) => {
+      console.error('MongoDB connection error:', err);
+      this.checkalive = false;
+    });
 
-        this.client.on('connect', () => {
-            console.log('Connected to MongoDB');
-            this.checkalive = true;
-        });
+    this.client.on('connect', () => {
+      console.log('Connected to MongoDB');
+      this.checkalive = true;
+    });
 
-        this.client.connect().catch(err => {
-            console.error('MongoDB connection error on initial connect:', err);
-        });
-    }
+    this.client.connect().catch((err) => {
+      console.error('MongoDB connection error on initial connect:', err);
+    });
+  }
 
-    isAlive() {
-        return true //this.checkalive;
-    }
+  isAlive() {
+    return this.client.isConnected(); // this.checkalive;
+  }
 
-    async nbUsers() {
-        const db = this.client.db()..collection('users').countDocuments();
-    }
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
 
-    async nbFiles() {
-	const db = this.client.db()..collection('files').countDocuments();
-    }
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
 
 const dbClient = new DBClient();
 
