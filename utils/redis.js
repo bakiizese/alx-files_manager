37db8f8b17c2a0a12daeb82a241dad576829fac3
bakiizese <bereketzeseselassie>
2024-06-17 00:46:01 -0700

    fixed esling

--- utils/redis.js
+++ utils/redis.js
@@ -2,32 +2,35 @@ import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-	constructor() {
-		this.checkalive = true
-		this.client = createClient();
-		this.client.on('error', (err) => {
-			this.checkalive = false
-			console.log(err);
-		})
-		this.client.on('connect', () => {
-			this.checkalive = true
-		})
-		this.getAsync = promisify(this.client.get).bind(this.client);
-	}
-	isAlive() {
-		return true;
-	}
+  constructor() {
+    this.checkalive = true;
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      this.checkalive = false;
+      console.log(err);
+    });
+    this.client.on('connect', () => {
+      this.checkalive = true;
+    });
+    this.getAsync = promisify(this.client.get).bind(this.client);
+  }
 
-	async get(key){
-		const val = await this.getAsync(key)
-		return val
-	}
-	async set(key, val, duration){
-		await this.client.setex(key, duration, val);
-	}
-	async del(key){
-		await this.client.del(key);
-	}
+  isAlive() {
+    return this.checkalive;
+  }
+
+  async get(key) {
+    const val = await this.getAsync(key);
+    return val;
+  }
+
+  async set(key, val, duration) {
+    await this.client.setex(key, duration, val);
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
 }
 
 const redisClient = new RedisClient();
