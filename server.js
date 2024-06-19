1e612bd2383349f2c65963d5e8c071e3092659c1
bakiizese <bereketzeselassie@gmail.com>
2024-06-18 23:42:14 -0700

    done with parentids

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const router = require('./routes/index');
 const bodyParser = require('body-parser');
+const router = require('./routes/index');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
